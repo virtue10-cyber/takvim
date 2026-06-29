c3e491675bdd57adaaad62c3b78d783895134955
virtue10-cyber <github_emailin@gmail.com>
2026-06-29 03:59:33 +0300

    Cache v3 - guncellemeleri zorla yukle

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'takvim-v2';
+const CACHE = 'takvim-v3';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
